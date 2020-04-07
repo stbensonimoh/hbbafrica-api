a9c9b14b92e941aeae61cbbd3cce3a231223cfcf
Benson Imoh,ST <benson@stbensonimoh.com>
2020-04-07 17:58:37 +0100

    Install npm packages and setup middleware

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,9 @@
 const port = <string | number> process.env.PORT || 3000;
 const hostname:any = '127.0.0.1';
 import { createServer, ServerResponse, IncomingMessage } from "http";
-// import { app } from "./routes/app";
+import { app } from "./routes/app";
 
-const server = createServer((req:IncomingMessage, res: ServerResponse):void => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({name: "Benson", message: "This is working eh?"}))
-
-});
+const server = createServer(app)
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
