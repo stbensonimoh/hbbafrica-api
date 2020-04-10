d661c96d3982cef8ff9cfbd9ddc726712acb5557
Benson Imoh,ST <benson@stbensonimoh.com>
2020-04-10 20:49:10 +0100

    Fix issue

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 const port = <string | number> process.env.PORT || 3000;
-const hostname:any = '127.0.0.1';
+// const hostname:any = '127.0.0.1';
 import { createServer, ServerResponse, IncomingMessage } from "http";
 import { app } from "./routes/app";
 
 const server = createServer(app)
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at ${port}`);
 });
